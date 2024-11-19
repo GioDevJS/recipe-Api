0298102430fcf8a9b338275473b23a60e985a8c2
GioDevJS <sgtech@live.com>
2024-11-18 23:02:58 -0300

    Added tests for adding users and panel for adding users

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin configuration for core app.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 from core import models
 
@@ -17,15 +18,17 @@ class UserAdmin(BaseUserAdmin):
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('name',)}),
         (
-            'Permissions',
+            _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        ('Important dates', {'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
+    readonly_fields = ['last_login']
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
+            'fields': ('email', 'password1', 'password2', 'name', 'is_staff', 'is_superuser')
         }),
     )
 
